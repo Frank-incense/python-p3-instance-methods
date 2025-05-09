e6ead30b1322ca5e8fd2fa5af418e2f0f6ed775e
Frankincense <frankincensewelsey@gmail.com>
2025-05-09 10:08:01 +0300

    classes in python

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,10 @@
 
 class Person:
     # Class body goes here
-
+    name = "Person"
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print(f"The {self.name.lower()} is walking.")
